cdb2dfb776b8ebccf0745c3cef7e434cec7a5ef0
Iliass <iliasselyaa@hotmail.com>
2021-04-12 15:16:15 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -13,10 +13,17 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
+        
     
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
 
-            if ($conn->query($sql) === TRUE) {
+        
+        //create query
+        $sql = "SELECT * FROM CREDENTIALS WHERE email = '$email' AND password = '$password'";
+        $result = $conn->query($sql);
+        if ($result->num_rows==1) {
                 echo "Success";
                  
             } else {
