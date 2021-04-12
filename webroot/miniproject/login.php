ec4a453c9aba0199c54d182b2e7e74ddc2456b8e
Iliass <iliasselyaa@hotmail.com>
2021-04-12 15:04:01 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -8,7 +8,7 @@
        $dbpwd = getenv("DATABASE_PASSWORD");
        $dbname = getenv("DATABASE_NAME");
         // Creates connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
         // Checks connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
