92f9d7311dbc162ffedc16b7566f08f8813395c5
Iliass <iliasselyaa@hotmail.com>
2021-04-12 15:01:11 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -2,9 +2,28 @@
 <html>
     <body>
         <?php
-        define("MESSAGE", "How are you?");
-        echo MESSAGE;
-        // Outputs "How are you?"
+       $dbhost = getenv("MYSQL_SERVICE_HOST");
+       $dbport = getenv("MYSQL_SERVICE_PORT");
+       $dbuser = getenv("DATABASE_USER");
+       $dbpwd = getenv("DATABASE_PASSWORD");
+       $dbname = getenv("DATABASE_NAME");
+        // Creates connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Checks connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+    
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Success";
+                 
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $conn->close();
+        }
         ?>
     </body>
 </html>
